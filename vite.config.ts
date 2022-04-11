dba3e802d3449fe8d06a7b17705531d5c156a8ae
Javier Echavez <jechavezch@gmail.com>
2022-04-11 15:50:06 -0500

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  base: '/libonati/',
+  plugins: [react()],
+});
