cefd4e9bcc59d5d9e57e5787390fd43c076c6a31
Javier Echavez <jechavezch@gmail.com>
2022-04-14 16:30:43 -0500

    GENERAL UPDATE :: Changed vite config to match netlify

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/libonati/',
-  plugins: [react()],
-});
+export default defineConfig({ plugins: [react()] });
